f8cadb6f4bbf90b43ff2babeb7e9da3fcb1f9f9e
Duc Luong <luong27052001@gmail.com>
2026-06-06 20:31:18 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  }
+  base: '/Repair-Military/', // Sửa thành chữ R và M viết hoa đúng theo tên thư mục của bạn
 })
